947a8ff38f1e3b113117d00997bed470b9b0c8d1
ruiwenge2 <85425620+ruiwenge2@users.noreply.github.com>
2021-08-29 00:24:20 +0000

    update main.py

--- main.py
+++ main.py
@@ -2,11 +2,8 @@ from flask import Flask, render_template
 app = Flask("app")
 
 @app.route("/")
-def home():
-  return render_template("home.html")
-
 @app.route("/home")
-def home2():
+def home():
   return render_template("home.html")
 
 @app.route("/projects")
